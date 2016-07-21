d463e13ec02c15cc6317047fb3e7f980799ee184
mirsakiyev <mirsakiyev@yahoo.com>
2016-07-21 19:33:07 -0400

    Created StudentDate.txt file with a set of information(name, surname. course, grades)

---
+++ COMP123-S2016_Assignment-5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace COMP123_S2016_Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
